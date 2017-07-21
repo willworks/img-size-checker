e4c4f6e1e8d31cd9d6b918d7e4c7b5cd431ac264
willworks <scnuzhonghanjin@gmail.com>
2017-07-21 20:52:50 +0800

    feat: init

---
+++ bin/checker.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+'use strict'
+
+const fs = require("fs")
+const program = require('commander');
+
+program
+    .option('-s, --size <size>', 'images max size', 'GK')
+    .option('-f, --file <file>', 'file', 'GK')
+
+const MAXSIZE = program.parse(process.argv).size || 300 * 1000;
+const file = program.parse(process.argv).file;
+
+fs.stat(file, function (err, stats) {
+  if (err) throw err;
+  if (stats.size > MAXSIZE) {
+    throw file + ' is over' + MAXSIZE + 'bit';
+  }
+});
