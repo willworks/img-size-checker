ced18416c15d0a48b5faaecab4d5cad78f1a76f2
willworks <scnuzhonghanjin@gmail.com>
2017-07-24 00:40:24 +0800

    feat: remove commander & upgrade v1.1.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # img-size-checker
-imags size checker use in pre-commit
+A images size checker use in pre-commit, you can config the maxsize in you package.json to prevent someone commit unhandle images to git repository
+
+
+### 1. install
+```
+npm i -D img-size-checker
+```
+
+### 2. use lint-staged
+
+```
+"lint-staged": {
+  "*.{jpg,png}": "img-size-checker 300" // size in bit
+}
+```
+
+### 3. config precommit script in package.json
+```
+"precommit": "lint-staged",
+```
+
+### 4. enjoy it!
--- bin/checker.js
+++ bin/checker.js
@@ -3,18 +3,15 @@
 'use strict'
 
 const fs = require("fs")
-const program = require('commander');
 
-program
-    .option('-s, --size <size>', 'images max size', 'GK')
-    .option('-f, --file <file>', 'file', 'GK')
+const maxsize = process.argv[2] || 300;
+const file = process.argv[4];
 
-const MAXSIZE = program.parse(process.argv).size * 1000 || 300 * 1000;
-const file = program.parse(process.argv).file;
-
-fs.stat(file, function (err, stats) {
+if (!file) throw 'not such file!';
+fs.stat(file, (err, stats) => {
   if (err) throw err;
-  if (stats.size > MAXSIZE) {
-    throw file + ' is over' + MAXSIZE + 'bit';
+  if (stats.size > maxsize) {
+    const msg = file + ' is over ' + maxsize + 'bit';
+    throw msg;
   }
 });
